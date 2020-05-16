d3798b0537d0a87d621f49ea76ed220e8869cf20
Hossein <me@ahmadpanah.net>
2020-05-16 21:23:47 +0430

    First Commit  | Bootstrap impl. done, Get Total Stats

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React , { useEffect , useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
+
+
+function App() {
+  const [latest, setLatest] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://corona.lmao.ninja/v2/all")
+      .then(res => {
+        setLatest(res.data);
+    })
+      .catch(err => {
+        console.log(err);
+    });
+  }, []);
+
+  const date = new Date(parseInt(latest.updated));
+  const lastUpdated = date.toString();
+
+  return (
+    <div className="App">
+    <CardDeck>
+    <Card bg="secondary" text="white" className="text-center" style={{margin: "10px"}}>
+      <Card.Body>
+        <Card.Title>Cases</Card.Title>
+        <Card.Text>
+          {latest.cases}
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+      <small>Last updated {lastUpdated}</small>
+      </Card.Footer>
+    </Card>
+    <Card bg="danger" text={"white"} className="text-center" style={{margin: "10px"}}>
+      <Card.Body>
+        <Card.Title>Deaths</Card.Title>
+        <Card.Text>
+        {latest.deaths}
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <small>Last updated {lastUpdated}</small>
+      </Card.Footer>
+    </Card>
+    <Card bg="success" text={"white"} className="text-center" style={{margin: "10px"}}>
+      <Card.Body>
+        <Card.Title>Recoverd</Card.Title>
+        <Card.Text>
+        {latest.recovered}
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <small>Last updated {lastUpdated}</small>
+      </Card.Footer>
+    </Card>
+  </CardDeck></div>
+  );
+}
+
+export default App;
